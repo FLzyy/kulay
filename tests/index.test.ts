afe6173ee9d7ef9f1b0e6b5948ea13feeed9474f
FLzyy <flzyy72@gmail.com>
2023-02-10 18:50:36 +0800

    fix tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,13 +6,13 @@ const text = ["Hello World", "!"];
 
 describe("kulay", () => {
   it("should return colorless when called directly", () => {
-    equal(kulay(...text), `${text.join()}`);
+    equal(kulay(...text), `${text.join("")}`);
   });
   for (const [key, value] of Object.entries(colors)) {
     it(`${key} === \\x1b[${value as number}m${text.join("")}\\x1b[0m`, () => {
       equal(
         kulay[key](...text),
-        `\x1b[${value as number}m${text.join("")}\x1b[0m`
+        `\x1b[${value as number}m${text.join("")}\x1b[0m`,
       );
     });
   }
